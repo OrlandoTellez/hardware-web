7ee901039cfb51bf9dbb12ed48c9831a1091be22
Orlando <espadachinkinno@gmail.com>
2025-03-08 15:32:35 -0600

    Add(header): agregar header y algunos estilos globales

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import styles from "./page.module.css";
+import { Header } from "@/components/Header";
 
 export default function Home() {
   return (
     <>
-    <div className={styles.container}>
-      <h1>Se empieza el proyecto</h1>
-    </div>
+      <Header />
+      <div className={styles.container}>
+        <h1>Se empieza el proyecto</h1>
+      </div>
     </>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'next/link'
+import "@/components/Header.css"
+
+export const Header = () => {
+  return (
+    <>
+        <header>
+            <div>
+                <h1>Hardware<span>Class</span></h1>
+            </div>
+            <nav>
+                <ul>
+                    <li><Link href="/">Inicio</Link></li>
+                    <li><Link href="/">Componentes</Link></li>
+                    <li><Link href="/">Principios</Link></li>
+                </ul>
+            </nav>
+        </header>
+    </>
+  )
+}
