7f387cb03106f28fe36f4b1eef5e14f00baa63ac
Orlando <espadachinkinno@gmail.com>
2025-03-08 21:24:33 -0600

    create(hero): crear seccion hero junto con sus estilos y agregar componentes

---
+++ src/sections/Hero.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styles from "./Hero.module.css";
+import { Button } from "@/components/Button";
+import { SecondaryButton } from "@/components/SecondaryButton";
+
+export const Hero = () => {
+  return (
+    <>
+      <section className={styles.hero}>
+        <article className={styles.text}>
+          <div>
+            <p>Bienvenido a HardwareClass</p>
+          </div>
+          <h1>
+            Descubre el <span> Universo del Hardware</span>
+          </h1>
+          <p className={styles.sub_text}>
+            Una plataforma educativa donde aprenderás todo sobre los componentes
+            de una computadora, su funcionamiento y los principios técnicos
+            detrás de la tecnología moderna.
+          </p>
+          <section className={styles.buttons}>
+            <Button text="Explorar componentes" url="/components" />
+            <SecondaryButton text="Ver principios tecnicos" url="/principios" />
+          </section>
+        </article>
+      </section>
+    </>
+  );
+};
