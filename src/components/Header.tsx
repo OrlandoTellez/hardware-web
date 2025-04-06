4bea7d7ec31ec3c6fec6544bcb8679666800e035
Orlando <espadachinkinno@gmail.com>
2025-04-06 00:03:05 -0600

    refactor: restructure Header component layout for improved organization and responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,21 +15,24 @@ export const Header = () => {
   return (
     <>
         <header className={styles.header}>
-            <div className={styles.logo}>
-                <h1>Hardware<span>Class</span></h1>
-            </div>
+            <div className={styles.headerContainer}>
+                <div className={styles.logo}>
+                    <h1>Hardware<span>Class</span></h1>
+                </div>
 
-            <div className={styles.bars} onClick={toggleMenu}>
-                <img src={bars.src} alt="bars" />
-            </div>
+                <div className={styles.bars} onClick={toggleMenu}>
+                    <img src={bars.src} alt="bars" />
+                </div>
 
-            <nav className={`${styles.nav} ${isOpen ? styles.active : ""}`}>
-                <ul className={styles.ul}>
-                    <li><Link href="/">Inicio</Link></li>
-                    <li><Link href="/componentes">Componentes</Link></li>
-                    <li><Link href="/principios">Principios</Link></li>
-                </ul>
-            </nav>
+                <nav className={`${styles.nav} ${isOpen ? styles.active : ""}`}>
+                    <ul className={styles.ul}>
+                        <li><Link href="/">Inicio</Link></li>
+                        <li><Link href="/componentes">Componentes</Link></li>
+                        <li><Link href="/principios">Principios</Link></li>
+                    </ul>
+                </nav>
+            </div>
+            
         </header>
     </>
   )
